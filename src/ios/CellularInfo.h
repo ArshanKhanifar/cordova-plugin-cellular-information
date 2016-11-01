db52da7230530dfe83c3a1387a51e838e192cd4c
arshan <arshankhanifar@gmail.com>
2016-11-01 12:01:25 -0400

    included depencencies

--- src/ios/CellularInfo.h
+++ src/ios/CellularInfo.h
@@ -1,7 +1,10 @@
 #import <Cordova/CDV.h>
 #import <CoreTelephony/CTCarrier.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
-
+#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDVInvokedUrlCommand.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
 @interface CellularInfo : CDVPlugin
 
 - (void)getCellularInfo:(CDVInvokedUrlCommand*)command;
