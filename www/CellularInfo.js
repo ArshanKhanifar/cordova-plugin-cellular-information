879a2944c91cb59ac690452ba6c5f47378583702
arshan <arshankhanifar@gmail.com>
2016-11-01 11:59:08 -0400

    adding get ip address functionality to cellularinfo

--- www/CellularInfo.js
+++ www/CellularInfo.js
@@ -1,5 +1,8 @@
 module.exports = {
   getCellularInfo: function(successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'CellularInfo', 'getCellularInfo', []);
+  },
+  getIPAddress: function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'CellularInfo', 'getIPAddress', []);
   }
 };
